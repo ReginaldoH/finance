fe031d8ce2d3ad306eec7e41cad1afa1c927393a
ReginaldoH <rhscamila@gmail.com>
2025-09-19 01:18:20 -0400

    ajuste modal

--- app/painel/paginas/calendario.php
+++ app/painel/paginas/calendario.php
@@ -9,10 +9,7 @@ if(@$calendario == 'ocultar'){
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-
-<!-- <link href='https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css' rel='stylesheet'> -->
-
+<!-- <link href='./css/bootstrap_5/bootstrap.css' rel='stylesheet'> -->
 <link href="paginas/fullcalendar/css/custom.css" rel="stylesheet">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,7 +17,7 @@ if(@$calendario == 'ocultar'){
 <div class="container" style="margin-top: 70px;">
   <div class="row align-items-center mt-3 mb-2">
     <div class="col-8">
-      <h4 class="m-0">📅 Agenda Receber</h4>
+      <h4 class="m-0">📅 Agenda Receber123</h4>
     </div>
     <div class="col-4 text-end">
       <!-- exemplo de select cliente (opcional) -->
@@ -40,7 +37,7 @@ if(@$calendario == 'ocultar'){
   <div class="modal-dialog modal-dialog-centered modal-lg">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title">Detalhes do dia</h5>
+        <h5 class="modal-title" id="detalheDia" class="me-auto fw-bold" >Detalhes do dia</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
       </div>
       <div class="modal-body">
@@ -55,14 +52,10 @@ if(@$calendario == 'ocultar'){
 </div>
 
 <!-- Bootstrap + FullCalendar JS (CDN) -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+ <!-- <script src="paginas/fullcalendar/js/bootstrap5/bootstrap.bundle.min.js"></script> -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script> -->
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.14/index.global.min.js"></script>
-
-
-    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
-    <!-- <script src='paginas/fullcalendar/js/index.global.min.js'></script> -->
-    <!-- <script src="paginas/fullcalendar/js/bootstrap5/index.global.min.js"></script> -->
-     
+   
 <script src='paginas/fullcalendar/js/core/locales-all.global.min.js'></script>
 <script src='paginas/fullcalendar/js/custom.js'></script>
 
